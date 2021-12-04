652aee038dae01ada08a1c07b266692a78fffe04
Faisalhossainerfan <67220051+Faisalhossainerfan@users.noreply.github.com>
2021-12-04 22:49:34 +0600

    Update and rename aosp_santoni.mk to exthm_santoni.mk

---
+++ exthm_santoni.mk
@@ -0,0 +1,31 @@
+# Inherit makefiles
+$(call inherit-product, device/xiaomi/santoni/full_santoni.mk)
+$(call inherit-product, vendor/exthm/config/common.mk)
+
+# ROM spesific makefile name
+PRODUCT_NAME := exthm_santoni
+PRODUCT_DEVICE := santoni
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi 4X
+BOARD_VENDOR := Xiaomi
+TARGET_VENDOR := Xiaomi
+PRODUCT_MANUFACTURER := Xiaomi
+
+# Gapps
+TARGET_GAPPS_ARCH := arm64
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+
+# Exthmui Stuff
+EXTHMUI_BUILD_TYPE := Unofficial
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 720
+	
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="santoni-user 7.1.2 N2G47H V9.5.10.0.NAMMIFD release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := "Xiaomi/santoni/santoni:7.1.2/N2G47H/V9.5.10.0.NAMMIFD:user/release-keys"
