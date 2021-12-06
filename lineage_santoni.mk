c041dcd0c4f1945c712a0723e65b04638d0ec5b6
Faisalhossainerfan <67220051+Faisalhossainerfan@users.noreply.github.com>
2021-12-07 02:37:04 +0600

    Update and rename exthm_santoni.mk to lineage_santoni.mk

--- lineage_santoni.mk
+++ lineage_santoni.mk
@@ -1,12 +1,24 @@
 # Inherit makefiles
 $(call inherit-product, device/xiaomi/santoni/full_santoni.mk)
-$(call inherit-product, vendor/exthm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
+# Touch HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.xiaomi_8937
+
+# Trust HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.trust@1.0-service
+
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm
+
 # ROM spesific makefile name
-PRODUCT_NAME := exthm_santoni
+PRODUCT_NAME := lineage_santoni
 PRODUCT_DEVICE := santoni
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 4X
@@ -18,9 +30,13 @@ PRODUCT_MANUFACTURER := Xiaomi
 TARGET_GAPPS_ARCH := arm64
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
 
-# Exthmui Stuff
-EXTHMUI_BUILD_TYPE := Unofficial
+# CipherOS Stuff
+CIPHEROS_BUILD_TYPE := Unofficial
 TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Maintainer
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.cipher.maintainer=Faisalhossainerfan
 
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 720
