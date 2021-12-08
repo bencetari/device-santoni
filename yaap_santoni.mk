0bd83eb5a940ab54c345f3ba8bd0f38c0fb8bc67
HyperN00B <bence.tari@gmail.com>
2021-12-08 20:48:08 +0100

    Clean this mess up

--- yaap_santoni.mk
+++ yaap_santoni.mk
@@ -1,15 +1,6 @@
 # Inherit makefiles
-$(call inherit-product, device/xiaomi/santoni/full_santoni.mk)
+$(call inherit-product, device/xiaomi/santoni/device.mk)
 $(call inherit-product, vendor/yaap/config/common_full_phone.mk)
-
-# ROM spesific makefile name
-PRODUCT_NAME := yaap_santoni
-
-# Gapps
-TARGET_USE_GAPPS := false
-TARGET_GAPPS_ARCH := arm64
-IS_PHONE := true
-
 
 # ROM spesific makefile name
 PRODUCT_NAME := yaap_santoni
